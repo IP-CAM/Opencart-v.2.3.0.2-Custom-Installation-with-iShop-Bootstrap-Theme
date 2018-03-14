30348874d1c93f8c937d52723a142e31acb6e82b
webistomin <webistomin@gmail.com>
2018-03-14 08:58:51 +0300

    Поправил некоторые скрипты. Не получается сделать многоуровневый дропдаун

--- src/js/submenu.js
+++ src/js/submenu.js
@@ -1,14 +1,1 @@
-var subToggle = document.querySelector('.main-nav__toggler');
-var subNav = document.querySelector('.main-nav__sublist');
-
-subToggle.addEventListener('click', function() {
-  if (subNav.classList.contains('main-nav__sublist--closed')) {
-    subNav.classList.remove('main-nav__sublist--closed');
-    subNav.classList.add('main-nav__sublist--opened');
-    subToggle.classList.add('main-nav__toggler__toggler--opened')
-  } else {
-    subNav.classList.add('main-nav__sublist--closed');
-    subNav.classList.remove('main-nav__sublist--opened');
-    subToggle.classList.remove('main-nav__toggler__toggler--opened');
-  }
-});
+// Пока не получается сделать
