a0c1ce599f24b064ed22510df681d02db8f75f57
webistomin <webistomin@gmail.com>
2018-03-16 15:41:29 +0300

    Сделал виджет инстаграмм

---
+++ src/js/widget.js
@@ -0,0 +1,11 @@
+var feed = new Instafeed({
+  get: 'user',      // <-- new
+  userId: '6725512930',   // <-- new
+  clientId: '218e4100d4a44d94b8a38e2dbab7e81d',
+  accessToken: '6725512930.218e410.24afc32b09b14c2eb3464b1dc3a4664f',
+  template: '<li class="widget__item"><a href="{{link}}"><img src="{{image}}" /></a></li>',
+  sortBy: 'most-recent',
+  limit: '10',
+  resolution: 'thumbnail'
+});
+feed.run();
