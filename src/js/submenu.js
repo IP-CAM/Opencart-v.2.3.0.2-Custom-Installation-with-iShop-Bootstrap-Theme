614cd5ee55bae4b1b38c47748a737d982c197047
webistomin <webistomin@gmail.com>
2018-05-02 08:56:04 +0300

    Добавил скрипт подменю

--- src/js/submenu.js
+++ src/js/submenu.js
@@ -1,1 +1,25 @@
-// Пока не получается сделать
+var dropdown = document.querySelectorAll('.dropdown');
+var dropdownArray = Array.prototype.slice.call(dropdown,0);
+
+dropdownArray.forEach(function(el) {
+  var button = el.querySelector('span.main-nav__toggler'),
+    menu = el.querySelector('.dropdown-menu');
+
+
+  button.onclick = function(event) {
+    if(!menu.hasClass('main-nav__sublist--opened')) {
+      menu.classList.add('main-nav__sublist--opened');
+      menu.classList.remove('main-nav__sublist--closed');
+      event.preventDefault();
+    }
+    else {
+      menu.classList.remove('main-nav__sublist--opened');
+      menu.classList.add('main-nav__sublist--closed');
+      event.preventDefault();
+    }
+  };
+});
+
+Element.prototype.hasClass = function(className) {
+  return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
+};
