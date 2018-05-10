ff37d76d915189cd82d14981ccd3524fc53625e0
Alexey Istomin <webistomin@gmail.com>
2018-05-10 12:17:06 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ishop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ishop/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OSPanel/domains/ishop/catalog/');
+define('DIR_SYSTEM', 'C:/OSPanel/domains/ishop/system/');
+define('DIR_IMAGE', 'C:/OSPanel/domains/ishop/image/');
+define('DIR_LANGUAGE', 'C:/OSPanel/domains/ishop/catalog/language/');
+define('DIR_TEMPLATE', 'C:/OSPanel/domains/ishop/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/OSPanel/domains/ishop/system/config/');
+define('DIR_CACHE', 'C:/OSPanel/domains/ishop/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OSPanel/domains/ishop/system/storage/download/');
+define('DIR_LOGS', 'C:/OSPanel/domains/ishop/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OSPanel/domains/ishop/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OSPanel/domains/ishop/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ishop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'ishop_');
